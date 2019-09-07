f9e0764a999404e381c02d911c69388571fea962
Derric Fragosa <fderric@gmail.com>
2019-09-07 21:29:54 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,36 @@
-def addition(num1, num2)
-
+def addition(num1,num2)
+  add = num1 + num2
+  puts add
 end
 
-def subtraction(num1, num2)
-
+def subtraction(num1,num2)
+  sub = num1 - num2
+  puts sub
 end
 
-def division(num1, num2)
-
+def division(num1,num2)
+  div = num1 / num2
+  puts div
 end
 
-def multiplication(num1, num2)
-
+def multiplication(num1,num2)
+  times = num1 * num2
+  puts times
 end
 
-def modulo(num1, num2)
-
+def modulo(num1,num2)
+  mod = num1 % num2
+  puts mod
 end
 
 def square_root(num)
+ puts Math.sqrt() #need help. it wont let me pass in argument 
+ end
 
-end
+
+addition(5,4)
+subtraction(10,5)
+division(50,2)
+multiplication(4,30)
+modulo(34,5)
+square_root(81)
